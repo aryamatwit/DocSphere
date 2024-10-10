68441cf0124f5bb11bdd43d4eb076980ff381fa8
jamesholly1 <jameswhjj@gmail.com>
2024-10-10 11:06:11 -0400

    Update client.py

--- client.py
+++ client.py
@@ -1,7 +1,28 @@
 import socket
+import threading
 
 HOST = '10.220.52.74' #local host
 PORT = 9999
+
+def receive_msg(client_socket):
+    while True:
+        try:
+            msg = client_socket.recv(1024).decode('utf-8')
+            if msg:
+                print(f"\n{msg}")
+            else:
+                break
+        except:
+            print("[ERROR] Lost connection to the server.")
+            break
+
+def send_msg(client_socket):
+    while True:
+        msg = input("You: ")
+        if msg.lower() == 'exit':
+             break
+        
+        client_socket.send(msg.encode('utf-8'))
 
 def start_client():
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -9,20 +30,11 @@ def start_client():
 
     welcomemsg = client.recv(1024).decode('utf-8')
 
-    while True:
-        try:
-            msg = input("You: ")
-            if msg.lower() == 'exit':
-                break
+    receive_thread = threading.Thread(target=receive_msg, args=(client,))
+    receive_thread.start()
 
-            client.send(msg.encode('utf-8'))
-            response = client.recv(1024).decode('utf-8')
-            print(f"Server: {response}")
-        
-        except socket.error as errormsg:
-            print(f"Communication error with server. {errormsg}")
-
-    client.close()
+    send_thread = threading.Thread(target=send_msg, args=(client,))
+    send_thread.start()
 
 if input("Would you like to connect to the server (yes/no)? ").lower() == 'yes':
     start_client()
