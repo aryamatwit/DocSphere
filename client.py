721087f64de346dd35763abd7fd44b989b277341
jamesholly1 <jameswhjj@gmail.com>
2024-10-10 10:25:04 -0400

    Update client.py

--- client.py
+++ client.py
@@ -1,4 +1,28 @@
 import socket
-import threading
-import sys
+
+import socket
+
+HOST = '10.220.52.74' #local host
+PORT = 9999
+
+def start_client():
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect((HOST,PORT))
+
+    welcomemsg = client.recv(1024).decode('utf-8')
+
+    while True:
+        msg = input("You: ")
+        if msg.lower == 'exit':
+            break
+
+        client.send(msg.encode('utf-8'))
+        response = client.recv(1024).decode('utf-8')
+        print(f"Server: {response}")
+
+    client.close()
+
+if input("Would you like to connect to the server: ").lower == 'yes':
+    start_client()
+
 
